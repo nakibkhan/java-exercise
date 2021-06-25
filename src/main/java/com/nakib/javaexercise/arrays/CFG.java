8edd9f45a9c10ec91de55b64a5ae0b698a275530
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-06-24 22:11:25 -0500

    (add) Find Key

---
+++ src/main/java/com/nakib/javaexercise/arrays/CFG.java
@@ -0,0 +1,46 @@
+package com.nakib.javaexercise.arrays;
+
+class GFG {
+    static boolean findxinkindowSize(int[] arr, int key, int size)
+    {
+        boolean result = false;
+
+        int batchSize = arr.length/size;
+
+        if(arr.length % size != 0)  {
+            batchSize++;
+        }
+
+        for (int batch = 0; batch < batchSize; batch++) {
+            int startIndex = batch * size;
+            int endIndex = startIndex + size;
+
+            if(batch == batchSize -1) endIndex = arr.length;
+
+            for (int i = startIndex; i <endIndex; i++)   {
+                System.out.println(arr[i]);
+                if(arr[i] == key)   {
+                    System.out.println("We have a match in a batch " + batch);
+                    result = true;
+                    break;
+                }
+
+                if(i == endIndex -1) return false;
+            }
+        }
+
+        return result;
+    }
+
+    // Driver Code
+    public static void main(String args[])
+    {
+        int arr[] = new int[] { 3, 5, 2, 4,  9,  3,
+                1, 7, 3, 11, 12, 3 };
+        int key = 3, size = 3;
+        if (findxinkindowSize(arr, key, size))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
---
+++ src/test/java/com/nakib/javaexercise/arrays/GFGTest.java
@@ -0,0 +1,44 @@
+package com.nakib.javaexercise.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GFGTest {
+
+    @Test
+    void findxinkindowSize_empty() {
+        int [] numberList = new int[] {};
+        Assertions.assertFalse(GFG.findxinkindowSize(numberList, 2, 2));
+    }
+
+    @Test
+    void findxinkindowSize_first() {
+        int [] numberList = new int[] {2, 1};
+        Assertions.assertTrue(GFG.findxinkindowSize(numberList, 2, 2));
+    }
+
+    @Test
+    void findxinkindowSize_first_key_less_Size() {
+        int [] numberList = new int[] {2, 1};
+        Assertions.assertTrue(GFG.findxinkindowSize(numberList, 2, 3));
+    }
+
+    @Test
+    void findxinkindowSize_multiple_Batch()    {
+        int [] numberList = new int[] {2, 1, 3, 2, 8, 9, 2};
+        Assertions.assertTrue(GFG.findxinkindowSize(numberList, 2, 3));
+    }
+
+    @Test
+    void findxinkindowSize_multiple_Batch_middle_batch_false()    {
+        int [] numberList = new int[] {2, 1, 3, 7, 8, 9, 2};
+        Assertions.assertFalse(GFG.findxinkindowSize(numberList, 2, 3));
+    }
+
+    @Test
+    void findxinkindowSize_multiple_Batch_end_batch_false()    {
+        int [] numberList = new int[] {2, 1, 3, 7, 2, 9, 1};
+        Assertions.assertFalse(GFG.findxinkindowSize(numberList, 2, 3));
+    }
+
+}

2e5345177b47a9ffcd4ce87830bd22e2002ae7f1
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-06-25 23:46:23 -0500

    (add) missing serial number

--- src/main/java/com/nakib/javaexercise/arrays/MissingSerialNumber.java
+++ src/main/java/com/nakib/javaexercise/arrays/MissingSerialNumber.java
@@ -1,12 +1,24 @@
 package com.nakib.javaexercise.arrays;
 
+import java.util.Arrays;
+
 class MissingSerialNumber {
-    static int missingNumber(int[] num) {
+    static int process(int[] num) {
+        Arrays.sort(num);
+        int beginNumber = num[0];
+
+        for (int i = 0; i< num.length; i++)    {
+            int expectedNum = beginNumber + i;
+            if (num[i] != expectedNum) return expectedNum;
+        }
+
         return -1;
     }
 
     public static void main(String[] args) {
+        int arr[] = new int[] { 1, 3, 2, 6, 5};
 
+        System.out.println("Missing Number is : " + process(arr));
     }
 
 }
